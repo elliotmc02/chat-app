2890d095034158d169b7b75792547ed32715d7b8
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2025-03-16 11:17:58 +0100

    refactor: optimize code and update styles if same user in previous message

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,6 +1,6 @@
 import { socket } from '@/socket';
 import { Message } from '@/types';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import { useSelectedUserStore } from '@/stores/selected-user';
 import { saveMessages, updateMessages } from '@/utils/functions';
 
@@ -9,6 +9,8 @@ export const Chat = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const { selectedUser } = useSelectedUserStore();
+
+  const storageKey = useMemo(() => selectedUser || 'global', [selectedUser]);
 
   const sendMessage = (e: React.FormEvent) => {
     e.preventDefault();
@@ -24,18 +26,15 @@ export const Chat = () => {
 
   useEffect(() => {
     setMessages([]);
-    const storageKey = selectedUser || 'global';
     const storedMessages = sessionStorage.getItem(storageKey);
     if (storedMessages) {
-      const parsedMessages: Message[] = JSON.parse(storedMessages);
-      setMessages(parsedMessages);
+      setMessages(JSON.parse(storedMessages));
     }
-  }, [selectedUser]);
+  }, [storageKey]);
 
   useEffect(() => {
-    const storageKey = selectedUser || 'global';
     saveMessages(storageKey, messages);
-  }, [messages, selectedUser]);
+  }, [messages, storageKey]);
 
   const onConnect = useCallback(() => {
     setIsConnected(true);
@@ -92,7 +91,7 @@ export const Chat = () => {
   ]);
 
   return (
-    <div className="bg-white dark:bg-gray-800 w-full rounded-r-xl flex flex-col items-center justify-center">
+    <div className="bg-white dark:bg-gray-800 w-full flex flex-col items-center justify-center">
       {!isConnected || !socket.id ? (
         <div className="flex flex-col items-center gap-4">
           <div className="w-12 h-12 border-t-4 border-blue-500 dark:border-teal-500 border-solid rounded-full animate-spin"></div>
@@ -100,64 +99,22 @@ export const Chat = () => {
         </div>
       ) : (
         <>
-          <h1 className="text-2xl dark:text-white uppercase my-5">
-            {selectedUser || 'Global'}
+          <h1 className="text-2xl dark:text-white uppercase my-5 font-bold">
+            {selectedUser.substring(0, 5) || 'GLOBAL'}
           </h1>
           <div className="flex flex-col h-full w-full overflow-y-auto my-4">
             {messages.map((data, index) => (
-              <div
+              <MessageBubble
                 key={index}
-                className={`max-w-xs md:max-w-sm py-1 px-3 mb-1 ${
-                  data.sender === socket.id
-                    ? 'self-end ml-auto'
-                    : 'self-start mr-auto'
-                }`}
-              >
-                <div className="flex flex-col">
-                  <div
-                    className={`flex gap-2 ${
-                      data.sender === socket.id
-                        ? 'justify-start flex-row-reverse'
-                        : 'justify-start'
-                    } items-center`}
-                  >
-                    <span
-                      className={`text-xs font-medium ${
-                        data.sender === socket.id
-                          ? 'text-blue-100 dark:text-teal-200'
-                          : 'text-gray-600 dark:text-gray-300'
-                      }`}
-                    >
-                      {data.sender === socket.id
-                        ? 'You'
-                        : data.sender.substring(0, 5)}
-                    </span>
-                    <span
-                      className={`text-xs ${
-                        data.sender === socket.id
-                          ? 'text-blue-100 dark:text-teal-200'
-                          : 'text-gray-500 dark:text-gray-400'
-                      }`}
-                    >
-                      {data.time}
-                    </span>
-                  </div>
-                  <span
-                    className={`rounded-xl px-1.5 py-1 ${
-                      data.sender === socket.id
-                        ? 'bg-blue-500 dark:bg-teal-800 text-white rounded-tr-none'
-                        : 'bg-gray-200 dark:bg-gray-700 dark:text-white rounded-tl-none'
-                    }`}
-                  >
-                    {data.text}
-                  </span>
-                </div>
-              </div>
+                message={data}
+                prevSender={messages[index - 1]?.sender}
+                isCurrentUser={data.sender === socket.id}
+              />
             ))}
           </div>
           <form
             onSubmit={sendMessage}
-            className="flex items-center justify-end gap-3 w-full border-t-3 p-4"
+            className="flex justify-end gap-3 w-full border-t-3 p-4"
           >
             <input
               type="text"
@@ -176,6 +133,68 @@ export const Chat = () => {
           </form>
         </>
       )}
+    </div>
+  );
+};
+
+const MessageBubble = ({
+  message,
+  prevSender,
+  isCurrentUser,
+}: {
+  message: Message;
+  prevSender: string | undefined;
+  isCurrentUser: boolean;
+}) => {
+  const isEqualSender = prevSender === message.sender;
+
+  return (
+    <div
+      className={`max-w-xs md:max-w-sm px-3 ${
+        isEqualSender ? 'pt-0.5 mt-0' : 'pt-2 mt-2'
+      } ${isCurrentUser ? 'self-end ml-auto' : 'self-start mr-auto'}`}
+    >
+      <div className="flex flex-col">
+        {!isEqualSender && (
+          <div
+            className={`flex gap-2 ${
+              isCurrentUser ? 'justify-start flex-row-reverse' : 'justify-start'
+            } items-center`}
+          >
+            <span
+              className={`text-xs font-medium ${
+                isCurrentUser
+                  ? 'text-blue-100 dark:text-teal-200'
+                  : 'text-gray-600 dark:text-gray-300'
+              }`}
+            >
+              {isCurrentUser ? 'You' : message.sender.substring(0, 5)}
+            </span>
+            <span
+              className={`text-xs ${
+                isCurrentUser
+                  ? 'text-blue-100 dark:text-teal-200'
+                  : 'text-gray-500 dark:text-gray-400'
+              }`}
+            >
+              {message.time}
+            </span>
+          </div>
+        )}
+        <span
+          className={`px-1.5 py-1 ${
+            isCurrentUser
+              ? `bg-blue-500 dark:bg-teal-800 text-white ${
+                  isEqualSender ? 'rounded-xl' : 'rounded-xl rounded-tr-none'
+                }`
+              : `bg-gray-200 dark:bg-gray-700 dark:text-white ${
+                  isEqualSender ? 'rounded-xl' : 'rounded-xl rounded-tl-none'
+                }`
+          }`}
+        >
+          {message.text}
+        </span>
+      </div>
     </div>
   );
 };
