58cb6f169c4c27bfd0267a8a27686e80b3fcb240
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2025-03-19 21:29:05 +0100

    chore: add new type definitions

--- frontend/src/types/index.d.ts
+++ frontend/src/types/index.d.ts
@@ -1,7 +1,19 @@
 export type Message = {
-    sender: string;
-    text: string;
-    date: string;
-    time: string;
-    recipient?: string;
-}
+  sender: string;
+  text: string;
+  date: string;
+  time: string;
+  recipient?: string;
+};
+
+export type Room = {
+  roomName: string;
+  users: string[];
+};
+
+export type Type = 'global' | 'user' | 'room';
+
+export type Chat = {
+  chat: string;
+  type: Type;
+};
